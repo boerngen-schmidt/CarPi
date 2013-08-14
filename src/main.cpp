bdf908fd84e6fba957e4cca8bb09418e501ce69a
Benjamin Börngen-Schmidt <boerngen-schmidt@stud.tu-darmstadt.de>
2013-08-15 00:41:38 +0900

    Added daemonizing and signal handling closes #9
    Main function now daemonizes itself
    Sigalhandling for SIGTERM was introduced
    Added documentation for functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,91 @@
 /*
  * 
  */
+#include <csignal>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/stat.h>
+
+
+/**
+ * @brief Indicator if program loop should continue running
+ */
+bool doRun = true;
+
+/**
+ * @brief Handles the signals
+ * 
+ * @param signal Numerical value of signal
+ * @return void
+ */
+void signalHandler(int signal) {
+	switch(signal) {
+		case SIGTERM:
+			doRun = false;
+			break;
+	}
+	return;
+}
+
 int main(int argc, char** argv) {
 
-    return 0;
+	pid_t pid, sid;
+	
+	/** 
+	 * Fork off the parent process
+	 * http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
+	 */
+	pid = fork();
+	if (pid < 0) {
+		// forking failed :(
+		exit(EXIT_FAILURE);
+	}
+	
+	if (pid > 0) {
+		// exit the parent process
+		exit(EXIT_SUCCESS);
+	}
+	
+	// From here on we are in the daemon process
+	/* Change the file mode mask */
+	umask(0);
+	
+	// TODO opening Logfiles for writing
+	
+	/* Create a new SID for the child process */
+	sid = setsid();
+	if (sid < 0) {
+        /* TODO Log any failure */
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Change the current working directory */
+	if ((chdir("/")) < 0) {
+		/* TODO Log any failure here */
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Close out the standard file descriptors */
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+	// DONE daemonizing.
+	
+	
+	/**
+	 * register the signalHandler for SIGTERM
+	 * http://www.cplusplus.com/reference/csignal/signal/
+	 */
+	void (*prev_fn)(int);
+	// pointer to the function which was previously in charge of handeling the signal
+	prev_fn = signal (SIGTERM,signalHandler);
+	if (prev_fn==SIG_IGN) {
+		signal (SIGTERM,SIG_IGN);
+	}
+
+	while(doRun) {
+		
+		sleep(2);
+	}
+	return 0;
 }
