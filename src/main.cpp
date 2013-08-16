ba9e4129567ef712dea3f680aa15f0dd67ff98c4
Benjamin Börngen-Schmidt <boerngen-schmidt@stud.tu-darmstadt.de>
2013-08-17 07:08:56 +0900

    Added program options closes #17
    Added the option to not daemonize the program

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
  */
 #include <csignal>
 #include <cstdlib>
+#include <iostream>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <syslog.h>
+#include <boost/program_options.hpp>
 #include "carpi.h"
 
+namespace po = boost::program_options;
 
+using namespace std;
 /**
  * @brief Indicator if program loop should continue running
  */
@@ -36,63 +40,87 @@ void signalHandler(int signal) {
 }
 
 int main(int argc, char** argv) {
-
-	pid_t pid, sid;
+	bool daemonize = true;
 	
-	/** 
-	 * Fork off the parent process
-	 * http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
-	 */
-	pid = fork();
-	if (pid < 0) {
-		syslog(LOG_ERR, "ERROR: Forking returned an error. Exiting.");
-		exit(EXIT_FAILURE);
+	try {
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help", "produce help message")
+			("nodaemon", "do not daemonize process")
+		;
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		
+		if (vm.count("help")) {
+			cout << desc << "\n";
+			return 0;
+        }
+        
+		if (vm.count("nodaemon")) {
+			daemonize = false;
+		}
+	}
+	catch (exception& e) {
+			cerr << "Error:" << e.what() << endl;
 	}
 	
-	if (pid > 0) {
-		syslog(LOG_DEBUG, "DEBUG: Exiting parent process. Forking was successful.");
-		exit(EXIT_SUCCESS);
+	if(daemonize) {
+		pid_t pid, sid;
+		
+		/** 
+		* Fork off the parent process
+		* http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
+		*/
+		pid = fork();
+		if (pid < 0) {
+			syslog(LOG_ERR, "ERROR: Forking returned an error. Exiting.");
+			exit(EXIT_FAILURE);
+		}
+		
+		if (pid > 0) {
+			syslog(LOG_DEBUG, "DEBUG: Exiting parent process. Forking was successful.");
+			exit(EXIT_SUCCESS);
+		}
+		
+		// From here on we are in the daemon process
+		/* Change the file mode mask */
+		umask(0);
+		
+		/* open syslog file */
+		setlogmask(LOG_UPTO(LOG_DEBUG));
+		openlog("CarPi", LOG_PID | LOG_ODELAY, LOG_USER);
+		
+		/* Create a new SID for the child process */
+		sid = setsid();
+		if (sid < 0) {
+			syslog(LOG_ERR, "ERROR: New SID for child could not be created. Exiting.");
+			exit(EXIT_FAILURE);
+		}
+		
+		/* Change the current working directory */
+		if ((chdir("/")) < 0) {
+			syslog(LOG_ERR, "ERROR: Could not change directory to '/'. Exiting.");
+			exit(EXIT_FAILURE);
+		}
+		
+		/* Close out the standard file descriptors */
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+		close(STDERR_FILENO);
+		syslog(LOG_DEBUG, "DEBUG: Daemonizing complete.");
+		
+		/**
+		* register the signalHandler for SIGTERM
+		* http://www.cplusplus.com/reference/csignal/signal/
+		*/
+		signal(SIGTERM, signalHandler); /* catch kill signal */
+		signal(SIGHUP, signalHandler); /* catch hangup signal */
+		signal(SIGCHLD, SIG_IGN); /* ignore child */
+		signal(SIGTSTP, SIG_IGN); /* ignore tty signals */
+		signal(SIGTTOU, SIG_IGN);
+		signal(SIGTTIN, SIG_IGN);
+		syslog(LOG_DEBUG, "DEBUG: Registered signal handling.");
 	}
-	
-	// From here on we are in the daemon process
-	/* Change the file mode mask */
-	umask(0);
-	
-	/* open syslog file */
-	setlogmask(LOG_UPTO(LOG_DEBUG));
-	openlog("CarPi", LOG_PID | LOG_ODELAY, LOG_USER);
-	
-	/* Create a new SID for the child process */
-	sid = setsid();
-	if (sid < 0) {
-        syslog(LOG_ERR, "ERROR: New SID for child could not be created. Exiting.");
-		exit(EXIT_FAILURE);
-	}
-	
-	/* Change the current working directory */
-	if ((chdir("/")) < 0) {
-		syslog(LOG_ERR, "ERROR: Could not change directory to '/'. Exiting.");
-		exit(EXIT_FAILURE);
-	}
-	
-	/* Close out the standard file descriptors */
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
-	syslog(LOG_DEBUG, "DEBUG: Daemonizing complete.");
-	
-	/**
-	 * register the signalHandler for SIGTERM
-	 * http://www.cplusplus.com/reference/csignal/signal/
-	 */
-	signal(SIGTERM, signalHandler); /* catch kill signal */
-	signal(SIGHUP, signalHandler); /* catch hangup signal */
-	signal(SIGCHLD, SIG_IGN); /* ignore child */
-	signal(SIGTSTP, SIG_IGN); /* ignore tty signals */
-	signal(SIGTTOU, SIG_IGN);
-	signal(SIGTTIN, SIG_IGN);
-	syslog(LOG_DEBUG, "DEBUG: Registered signal handling.");
-
 	carpi = new CarPi();
 	carpi->run();
 	// after return from run we free the memory space
